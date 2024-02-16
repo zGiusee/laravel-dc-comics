518ece442cc77de170c7962ad1cbeeb628105342
zGiusee <giusebelforte@gmail.com>
2024-02-16 14:00:38 +0100

    [UPDATE] created migration

---
+++ database/migrations/2024_02_16_125219_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->text('description');
+            $table->text('thumb');
+            $table->string('price', 20);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 30);
+            $table->text('artist');
+            $table->text('writers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
